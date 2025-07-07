525331ee81db0ad89c3609b7032572555da7dd5c
ZIGA GOBEC <ziga.gobec@pro-bit.si>
2025-07-07 13:57:46 +0200

    added api

---
+++ api/main.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get("/api/v1/health", (req, res) => {
+  res.json({
+    message: "ok",
+    sok: "pomarančni"
+  });
+});
+
+app.listen(PORT, () => {
+    console.log("App is running")
+});
